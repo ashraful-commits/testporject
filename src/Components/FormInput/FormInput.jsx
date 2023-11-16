c618df0debf513f36dbadee10b33103d34bb4e32
ashraful-commits <ashrfulalam10000@gmail.com>
2023-11-17 04:29:43 +0600

    updated

--- src/Components/FormInput/FormInput.jsx
+++ src/Components/FormInput/FormInput.jsx
@@ -14,7 +14,10 @@ const FormInput = ({
       initial={{ y: -15, opacity: 0.3 }}
       animate={{ y: 0, opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 1, delay: 0.3, type: "spring", stiffness: 200 }}
+      transition={{
+        duration: 0.3,
+        delay: 0.1 * Math.random() * 10,
+      }}
       className="w-full flex flex-col items-center hover:scale-105 transition-all duration-500 ease-in-out"
     >
       {/* ==========================================label  */}
