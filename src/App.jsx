17edb23a898f374d3f88a0b0f637c82010be2fb2
ashraful-commits <ashrfulalam10000@gmail.com>
2023-12-20 02:11:47 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import PageRouter from "./Routers/PageRouter";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { LoggedInSeller, getAllSeller } from "./Features/Seller/SellerApi";
+import { useDispatch } from "react-redux";
+import { LoggedInSeller } from "./Features/Seller/SellerApi";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AnimatePresence } from "framer-motion";
